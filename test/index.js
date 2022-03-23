b43e73868a4cf312bed8108b94643e8de384ca47
ggmkimjiseong613 <ggm.kimjiseong613@gmail.com>
2022-03-23 19:23:55 +0900

    2022-03-23

--- test/index.js
+++ test/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const ws = require('ws');
+
+const server = new ws.Server({port : 8080});
+
+server.on("listening", () => {
+    console.log("서버가 열렸습니다");
+
+});
+
+server.on("connection", (socket) => {
+
+    socket.on("message",(message) => {
+        socket.send(message.toString());
+        
+    });
+});
