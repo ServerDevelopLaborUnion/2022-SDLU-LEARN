4a2143fcca0f68b8816a5e6d0d90c774486fb29c
Nam junseong <57340097+danielnam0801@users.noreply.github.com>
2022-03-23 19:23:20 +0900

    20220323

--- test/index.js
+++ test/index.js
@@ -1,1 +1,13 @@
-const name = "남준성";
+const ws = require("ws");
+
+const server = new ws.Server({port:8080});
+
+server.on("listening",()=>{
+    console.log("서버가 열렸습니다!");
+});
+
+server.on("connection", (socket) => {
+   socket.on("message", (message)=>{
+        socket.send(message.toString());
+    });
+});
