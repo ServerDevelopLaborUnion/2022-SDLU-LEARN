91583ee4eaa1547654bc7fc91417b555feaf6981
KBluePurple <42506968+BlWon@users.noreply.github.com>
2022-03-23 19:22:44 +0900

    2022-03-23

--- test/index.js
+++ test/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const ws = require('ws');
+
+const server = new ws.Server({port: 8080});
+
+server.on("listening", () => {
+    console.log("서버가 열렸습니다!");
+});
+
+server.on("connection", (socket) => {
+    socket.on("message", (message) => {
+        socket.send(message.toString());
+    });
+});
