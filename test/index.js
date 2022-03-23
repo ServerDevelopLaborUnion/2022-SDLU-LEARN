eedd3705c35d4bbd0f79784d3535e06c55adec5c
dksgudwn <dksgudwn620@gmail.com>
2022-03-23 18:58:12 +0900

    서버열기

--- test/index.js
+++ test/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const ws = require('ws');
+
+const server = new ws.Server({port: 8080});
+
+server.on("listening", () => {
+    console.log("서버가 열렸습니다!");
+});
+
+server.on("connection", (socket) =>{
+    socket.send("Hello World");
+
+    socket.on("message", (message) => {
+        console.log(message.tostring());
+    });
+});
