b9e6b7160802ee12fc7e648f57dc858f3745c5a3
damin06 <ko060614@gmail.com>
2022-03-23 19:23:10 +0900

    2022-03-23

--- test/index.js
+++ test/index.js
@@ -1,3 +1,16 @@
-console.log("Hello World!");
+console.log("Hello World");
+const { Socket } = require('dgram');
+const ws =require('ws');
 
-console.log()
+const server = new ws.Server({port: 8080});
+
+server.on("listening", () =>{
+    console.log("서버가 열렸습니다!");
+});
+server.on("connection", (Socket) =>{
+Socket.send("Hello World!");
+
+Socket.on("mesage", (message)=> {
+console.log(message.tostring());
+ });
+});
