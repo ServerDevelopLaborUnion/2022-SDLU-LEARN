ee4a9773ff84ab56aa16688b2ad276ab4d474c58
성지은 <sungjieun.dev@gmail.com>
2022-03-24 22:33:46 +0900

    2022-03-24_

--- test/index.js
+++ test/index.js
@@ -8,8 +8,11 @@ server.on("listening", () => { //listening == 서버가 열렸을 때
 
 server.on("connection", (soket) => { 
     soket.send("Hello World"); //클라한테 메시지 보내기
+    console.log("클라이언트가 접속했습니다!");
 
-    soket.on("message", (message) => {
+    soket.on("message", (message) => { //메시지 이벤트
+    
+        console.log(message.toString());
         soket.send(message.toString()); 
     });
 });
