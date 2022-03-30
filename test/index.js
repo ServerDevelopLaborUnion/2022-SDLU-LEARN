2c24584e354fb66c01becdede9ed87e370325373
Nam junseong <57340097+danielnam0801@users.noreply.github.com>
2022-03-30 20:33:55 +0900

    3/30

--- test/index.js
+++ test/index.js
@@ -1,15 +1,19 @@
+const { WebSocketServer } = require("ws");
 const ws = require("ws");
 
 const server = new ws.Server({port:8080});
 
+let number = 0;
 server.on("listening",()=>{
     console.log("서버가 열렸습니다!");
 });
 
 server.on("connection", (socket) => {
     console.log("클라이언트가 접속했습니다!");
-    socket.on("message", (message)=>{
-        console.log(message.toString());
-        socket.send(message.toString());
+    client.id = number++;
+    client.on("message", (message)=>{
+        Wss.clients.forEach((client)=> {
+            client.send(client.id + ": " + message.toString());
+        });
     });
 });
